4fd63e684eaabaf10a2708ca72e732a447229ede
Vladislav1804vV <suhoverhov.vladislav1804@gmail.com>
2020-12-14 17:19:53 +0400

    Исправил некоторые неточности в скрипте для построения своей БД

--- script.sql
+++ script.sql
@@ -1,45 +1,43 @@
 CREATE DATABASE `myAirport` ;
-USE `myAirport` ;
 
-CREATE TABLE `countries` (
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(45) NOT NULL,
-    PRIMARY KEY (`id`)
-);
+CREATE TABLE `myAirport`.`countries` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`)) ;
 
-CREATE TABLE `cities` (
+CREATE TABLE `myAirport`.`cities` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `country_id` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
     FOREIGN KEY (`country_id`)
-    REFERENCES `countries` (`id`)) ;
+    REFERENCES `myAirport`.`countries` (`id`)) ;
 
-CREATE TABLE `airports` (
+CREATE TABLE `myAirport`.`airports` (
   `code` VARCHAR(4) NOT NULL,
   `name` VARCHAR(45) NOT NULL,
   `type` ENUM("INTL", "DOM") NOT NULL,
   `city_id` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`code`),
     FOREIGN KEY (`city_id`)
-    REFERENCES `cities` (`id`)) ;
+    REFERENCES `myAirport`.`cities` (`id`)) ;
 
-CREATE TABLE `gates` (
+CREATE TABLE `myAirport`.`gates` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `number` INT UNSIGNED NOT NULL,
   `type` ENUM("SMALL", "LARGE", "MEDIUM") NOT NULL,
   `airport_code` VARCHAR(4) NOT NULL,
   PRIMARY KEY (`id`),
     FOREIGN KEY (`airport_code`)
-    REFERENCES `airports` (`code`)) ;
+    REFERENCES `myAirport`.`airports` (`code`)) ;
 
-CREATE TABLE `planes` (
+CREATE TABLE `myAirport`.`planes` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `model` VARCHAR(45) NOT NULL,
-  `tailNumber` INT UNSIGNED NOT NULL,
+  `tailNumber` VARCHAR(10) NOT NULL,
   PRIMARY KEY (`id`)) ;
 
-CREATE TABLE `flights` (
+CREATE TABLE `myAirport`.`flights` (
   `number` VARCHAR(7) NOT NULL,
   `kind` ENUM("REGULAR", "ADDITIONAL", "SPECIAL", "CHARTER") NOT NULL,
   `type` ENUM("NON-STOP", "DOCKING", "STRAIGHT") NOT NULL,
@@ -52,13 +50,36 @@ CREATE TABLE `flights` (
   `plane_id` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`number`),
     FOREIGN KEY (`gate_id_in`)
-    REFERENCES `gates` (`id`),
+    REFERENCES `myAirport`.`gates` (`id`),
     FOREIGN KEY (`plane_id`)
-    REFERENCES `planes` (`id`),
+    REFERENCES `myAirport`.`planes` (`id`),
     FOREIGN KEY (`gate_id_out`)
-    REFERENCES `gates` (`id`)) ;
+    REFERENCES `myAirport`.`gates` (`id`)) ;
+    
+CREATE TABLE `myAirport`.`discounts` (
+  `discountСode` VARCHAR(10) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `desription` TEXT(255) NOT NULL,
+  `amount` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`discountСode`)) ;
 
-CREATE TABLE `sits` (
+CREATE TABLE `myAirport`.`baggages` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `weight` DOUBLE UNSIGNED NOT NULL,
+  `volume` DOUBLE UNSIGNED NOT NULL,
+  `type` ENUM("CARRYON", "SUITCASE", "ANIMAL") NOT NULL,
+  PRIMARY KEY (`id`)) ;
+
+CREATE TABLE `myAirport`.`clients` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `FIO` VARCHAR(255) NOT NULL,
+  `passportData` VARCHAR(100) NOT NULL,
+  `age` INT UNSIGNED NOT NULL,
+  `gender` ENUM('M', 'F') NOT NULL,
+  `account` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`)) ;
+
+CREATE TABLE `myAirport`.`sits` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `number` INT UNSIGNED NOT NULL,
   `class` ENUM('A', 'B', 'C') NOT NULL,
@@ -67,45 +88,22 @@ CREATE TABLE `sits` (
   `plane_id` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
     FOREIGN KEY (`plane_id`)
-    REFERENCES `planes` (`id`)) ;
+    REFERENCES `myAirport`.`planes` (`id`)) ;
 
-CREATE TABLE `discounts` (
-  `discountСode` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `desription` TEXT(255) NOT NULL,
-  `amount` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`discountСode`)) ;
-
-CREATE TABLE `baggages` (
+CREATE TABLE `myAirport`.`tickets` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `weight` DOUBLE UNSIGNED NOT NULL,
-  `volume` DOUBLE UNSIGNED NOT NULL,
-  `type` ENUM("CARRYON", "SUITCASE", "ANIMAL") NOT NULL,
-  PRIMARY KEY (`id`)) ;
-
-CREATE TABLE `clients` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `FIO` VARCHAR(255) NOT NULL,
-  `passportData` VARCHAR(100) NOT NULL,
-  `age` INT UNSIGNED NOT NULL,
-  `gender` ENUM('M', 'F') NOT NULL,
-  `account` VARCHAR(45) NOT NULL,
-  `ticket_id` INT NOT NULL,
-  PRIMARY KEY (`id`)) ;
-  
-CREATE TABLE `tickets` (
-  `id` INT NOT NULL AUTO_INCREMENT,
   `purchaseDate` DATETIME NOT NULL,
-  `sit_id` INT UNSIGNED NOT NULL,
-  `sales_discountСode` INT UNSIGNED NOT NULL,
+  `cost` DOUBLE UNSIGNED NOT NULL,
+  `sales_discountСode` VARCHAR(10) NOT NULL,
   `baggage_id` INT UNSIGNED NOT NULL,
   `client_id` INT UNSIGNED NOT NULL,
+  `sit_id` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
-    FOREIGN KEY (`sit_id`)
-    REFERENCES `sits` (`id`),
     FOREIGN KEY (`sales_discountСode`)
-    REFERENCES `discounts` (`discountСode`),
+    REFERENCES `myAirport`.`discounts` (`discountСode`),
     FOREIGN KEY (`baggage_id`)
-    REFERENCES `baggages` (`id`),
+    REFERENCES `myAirport`.`baggages` (`id`),
     FOREIGN KEY (`client_id`)
-    REFERENCES `clients` (`id`)) ;
+    REFERENCES `myAirport`.`clients` (`id`),
+    FOREIGN KEY (`sit_id`)
+    REFERENCES `myAirport`.`sits` (`id`)) ;
